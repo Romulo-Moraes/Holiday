74615eb417dfe35317cbb461143ebd81271be6e8
Romulo-Moraes <romuloperes6@gmail.com>
2022-10-09 20:20:35 +0000

    Error message customization added

--- Holiday/codes/parseArguments.c
+++ Holiday/codes/parseArguments.c
@@ -64,7 +64,8 @@ int checkIfAllRequiredArgumentsWasGiven(argParserData *data){
 
             for(int j = 0; j < data->allCollectedOptionalArgumentsIndex; j++){
 
-                if(strcmp(data->allCollectedOptionalArguments[j].longArgumentName, data->necessaryOptionalArguments[i].longArgumentName) == 0 || strcmp(data->allCollectedOptionalArguments[j].shortArgumentName, data->necessaryOptionalArguments[i].shortArgumentName) == 0){
+                if(strcmp(data->all
+                [j].longArgumentName, data->necessaryOptionalArguments[i].longArgumentName) == 0 || strcmp(data->allCollectedOptionalArguments[j].shortArgumentName, data->necessaryOptionalArguments[i].shortArgumentName) == 0){
                     found = TRUE;
                 }
             }
